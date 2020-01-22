cf193c512b97015be0b9d51a4dcf56f33daca6aa
karla karla <karolinakrauze9@gmail.com>
2020-01-22 15:44:20 +0100

    recipe layout

--- app/src/main/java/com/drinkingTeam/Singleton.java
+++ app/src/main/java/com/drinkingTeam/Singleton.java
@@ -10,7 +10,7 @@ import com.drinkingTeam.drinkingProject.R;
 public final class Singleton {
 
     private static final Singleton INSTANCE = new Singleton();
-    public static final String HOST = "http://192.168.0.38:8080";
+    public static final String HOST = "http://192.168.0.27:8080";
     public static final String GET_DRINKS = "/get/drinks";
     public static final String LOGIN = "/user/token";
     public static final String REGISTER = "/user/register";
